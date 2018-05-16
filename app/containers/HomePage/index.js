2fa785711d78e89530d28c2af357e772feeb7fbc
Pedro Luís <pedro@gymhopper.com>
2018-05-16 21:16:22 +0100

    Initial commit

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,13 +12,35 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <nav className="navbar navbar-expand-md navbar-light fixed-top bg-white border-bottom box-shadow">
+        <a className="navbar-brand" href="#">Carousel</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+          <a className="nav-link" href="#">Login<span className="sr-only">(current)</span></a>
+          <a className="btn btn-outline-primary my-2 my-sm-0">Sign up today</a>
+        </div>
+      </nav>
+
+
     );
   }
 }
+
