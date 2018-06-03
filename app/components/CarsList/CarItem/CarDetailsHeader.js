8e1ef8249d191ca8ce8aac58a243b0fb5bec1e0f
Pedro Luís <pedro@gymhopper.com>
2018-06-03 15:13:39 +0100

    Improve available_from date formatting

--- app/components/CarsList/CarItem/CarDetailsHeader.js
+++ app/components/CarsList/CarItem/CarDetailsHeader.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
+
+function getAvailableStartDate(car) {
+  return moment(car.available_start_date).format('Do MMMM YYYY');
+}
 
 const CarDetailsHeader = props => (
   <div className="d-flex flex-row justify-content-between p-2">
     <div className="d-flex flex-column">
       <div className="font-weight-bold">
-        {props.car.vehicle_make + ' ' + props.car.vehicle_model}
+        {`${props.car.vehicle_make}  ${props.car.vehicle_model}`}
       </div>
       <div className="">
         Located in {props.car.postcode}
       </div>
     </div>
 
-    Available from {props.car.available_start_date}
+    Available from {getAvailableStartDate(props.car)}
   </div>
 );
 
@@ -20,6 +25,8 @@ CarDetailsHeader.propTypes = {
   car: PropTypes.shape({
     postcode: PropTypes.string,
     available_start_date: PropTypes.string,
+    vehicle_make: PropTypes.string,
+    vehicle_model: PropTypes.string,
   }).isRequired,
 };
 
